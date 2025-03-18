4d9b63f2e0c6e40e32f62c6bc9c1f8a5d70cf34c
wishcrafter <wishcrafter.corp@gmail.com>
2025-03-18 23:08:12 +0900

    Update oq_login.py

--- oq_login.py
+++ oq_login.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 app = Flask(__name__)
 
@@ -17,17 +16,16 @@ def home():
 def login():
     # Chrome Driver 설정
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Headless 모드로 실행
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
     # Chrome Driver 실행
-    driver_path = "/usr/bin/chromedriver"  # Render 배포시 ChromeDriver 경로
+    driver_path = "/usr/bin/chromedriver"  # Render 환경에서는 이 경로 사용
     service = Service(driver_path)
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     try:
-        # 오더퀸 로그인 페이지 접속
         url = "https://www.orderqueen.kr/backoffice_admin/login.itp"
         driver.get(url)
 
@@ -38,10 +36,8 @@ def login():
         # 로그인 버튼 클릭
         driver.find_element(By.ID, "btnLoginNew").click()
 
-        # 성공적으로 이동할 때까지 대기
+        # 쿠키 추출
         WebDriverWait(driver, 10).until(EC.url_contains("backoffice_admin"))
-
-        # 쿠키 확인
         cookies = driver.get_cookies()
         session_cookie = next(cookie['value'] for cookie in cookies if cookie['name'] == 'SESSION')
 
